cf306fa1f3f507d2aaa83edae25bc94fe6aebb69
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-01 17:08:16 +0200

    Update Zanras.php

--- models/Zanras.php
+++ models/Zanras.php
@@ -17,42 +17,4 @@ class Zanras{
             $this->pavadinimas = $row['pavadinimas'];
         }
     }
-    //Paima daug elemetų iš duombazės pagal pateiktą sąlygą
-    public function selectMany($where = null){
-        if($where == null){
-            $where = "";
-        }else{
-            $where = " WHERE ".$where;
-        }
-        $result = [];
-        $query = "SELECT * FROM zanras".$where;
-        if($result = mysqli_query($this->mysqli, $query)) {
-            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                $temp = new zanras($row['pavadinimas']);
-                $temp->id = $row['id'];
-                $result[] = $temp;
-            }
-        }
-        return $result;
-    }
-    //Įterpia elementą į duombazę
-    public function insert(){
-        $query =  "INSERT INTO zanras (pavadinimas) VALUE (
-          '".mysqli_real_escape_string($this->mysqli, $this->pavadinimas)."
-          
-        )";
-        mysqli_query($this->mysqli, $query);
-    }
-    //Atnaujina elementą duombazėje
-    public function update(){
-        $query = "UPDATE zanras SET 
-          pavadinimas='".$this->pavadinimas."'
-          WHERE id = ".$this->id;
-        mysqli_query($this->mysqli, $query);
-    }
-    //Ištrina elementą iš duombazės
-    public function remove(){
-        $query = "DELETE FROM zanras WHERE id = ".$this->id;
-        mysqli_query($this->mysqli, $query);
-    }
 }
