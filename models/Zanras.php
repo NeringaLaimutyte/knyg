2f74554720d21f5be13704a27a60f05945427dbb
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-01 17:08:32 +0200

    Update Zanras.php

--- models/Zanras.php
+++ models/Zanras.php
@@ -8,13 +8,4 @@ class Zanras{
         $this->mysqli = $mysqli;
         $this->pavadinimas = $pavadinimas;
     }
-    //Paima 1 elementą iš duombazės pagal jo ID
-    public function select($id){
-        $query =  "SELECT * FROM zanras WHERE id = ".$id;
-        if($result = mysqli_query($this->mysqli, $query)) {
-            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            $this->id = $row['id'];
-            $this->pavadinimas = $row['pavadinimas'];
-        }
-    }
 }
