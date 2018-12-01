846016fa1a4da47d8ae5230e153d413e5ea418d6
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-01 17:20:44 +0200

    Create Autoriai.php

---
+++ models/Autoriai.php
@@ -0,0 +1,13 @@
+﻿<?php
+class Autoriai{
+    public $id;
+    public $fk_Knyga;
+    public $fk_Autorius;
+    public $mysqli;
+    //Autorius konstruktorius.
+    function __construct($mysqli, $fk_Knyga = '', $fk_Autorius = '') {
+        $this->mysqli = $mysqli;
+        $this->fk_Knyga = $fk_Knyga;
+        $this->fk_Autorius = $fk_Autorius;
+    }
+}
