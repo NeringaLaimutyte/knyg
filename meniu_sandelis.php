e43ffc9eb8ccc379461aab400fe5d67cbf16777e
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-01 19:19:28 +0200

    Create meniu_sandelis.php

---
+++ meniu_sandelis.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 2pt;
+    padding: 0;
+    overflow: hidden;
+    background-color: #90EE90;
+    font-size:130%;
+    font-family:'Times New Roman', Times, serif;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: #696969;
+    text-align: center;
+    padding: 18px 16px;
+    text-decoration: none;
+    
+}
+
+li a:hover:not(.active) {
+    color: white;
+    background-color:MediumSeaGreen;;
+}
+
+.active {
+    color: white;
+    background-color: rgb(95, 194, 98);
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li ><a href="#home">TIESIOG KNYGYNAS</a></li>
+  <li style="float:right"><a href="#login">Užsakymas</a></li>
+  <li style="float:right"><a href="#search">Priėmimas</a></li>
+  <li style="float:right"><a href="#news">Išdavimas</a></li>
+  <li style="float:right"><a href="#books">Inventorizacija</a></li>
+  <li style="float:right"><a href="#books">Likučio peržiūra</a></li>
+  <li style="float:right"><a href="#books">Ataskaitos</a></li>
+  
+  
+  
+</ul>
+
+</body>
+</html>
