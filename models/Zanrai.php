e542d2c200a07362e89dec4b9bd8fd92e308e1cc
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-01 17:21:00 +0200

    Create Zanrai.php

---
+++ models/Zanrai.php
@@ -0,0 +1,13 @@
+﻿<?php
+class Zanrai{
+    public $id;
+    public $fk_Knyga;
+    public $fk_zanras;
+    public $mysqli;
+    //zanras konstruktorius.
+    function __construct($mysqli, $fk_Knyga = '', $fk_zanras = '') {
+        $this->mysqli = $mysqli;
+        $this->fk_Knyga = $fk_Knyga;
+        $this->fk_zanras = $fk_zanras;
+    }
+}
