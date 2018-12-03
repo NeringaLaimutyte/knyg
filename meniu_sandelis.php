455aa5351c7961e770875d74bb6535685379505d
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-03 21:28:17 +0200

    Update and rename meniu_sandelis.html to meniu_sandelis.php

---
+++ meniu_sandelis.php
@@ -0,0 +1,23 @@
+﻿<?php
+include_once 'models/Vartotojas.php';
+?>
+<ul>
+    <li ><a href="sandelisMeniu.php">TIESIOG KNYGYNAS</a></li>
+  <?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    $array = [];
+    $array[] = ['Atsijungti', 'logout.php'];
+    $array[] = ['Ataskaitos', '#'];
+    $array[] = ['Likučio peržiūra', '#'];
+    $array[] = ['Inventorizacija', '#'];
+
+  for ($i = 0; $i < count($array); $i++) {
+      echo "<li style='float:right'><a href='" . $array[$i][1] . "'>" . $array[$i][0] . "</a></li>";
+  }
+  ?>
+  
+  
+  
+</ul>
