2dc15d63554d9b46c6c4ab7e70b827c1efadd3c5
Voldemortas <eragonas5@gmail.com>
2018-12-03 18:58:42 +0200

    Pridėjau log.php

---
+++ models/Logas.php
@@ -0,0 +1,17 @@
+﻿<?php
+class Logas{
+    public $id;
+    public $data;
+    public $IP;
+    public $URL;
+    public $laikas;
+    public $fk_Vartotojas;
+    //Logo konstruktorius.
+    function __construct($data = 0, $IP = '0.0.0.0', $URL = '/', $laikas = '', $fk_Vartotojas = 0) {
+        $this->data = $data;
+        $this->IP = $IP;
+        $this->URL = $URL;
+        $this->laikas = $laikas;
+        $this->fk_Vartotojas = $fk_Vartotojas;
+    }
+}
