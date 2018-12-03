952e3090e4ad12d9d11088cf8fb4e4e997e39762
NeringaLaimutyte <36457048+NeringaLaimutyte@users.noreply.github.com>
2018-12-03 21:28:52 +0200

    Create sandelisMeniu.php

---
+++ sandelisMeniu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+    include_once 'log.php';
+    include 'meniu_sandelis.php';
+?>
+<h1>Sveiki atvykę į</h1>
+<h1>TIESIOG KNYGYNAS</h1>
+<span class="container">
+    <img src="images/knygos.jpg" alt="Užsakymas" class="image" />
+    <div class="middle">
+      <a href="#" class="button">Užsakymas</a>
+    </div>
+  </span>
+  <span class="container">
+    <img src="images/naujienos.jpg" alt="Priėmimas" class="image" />
+    <div class="middle">
+      <a href="#" class="button">Priėmimas</a>
+    </div>
+  </span>
+  <span class="container">
+    <img src="images/ieškotiknygos.jpg" alt="Išdavimas" class="image" />
+    <div class="middle">
+      <a href="#" class="button">Išdavimas</a>
+    </div>
+  </span>
+</body>
+</html>
